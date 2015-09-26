ff4d321257c68bd9d7a930a6108631173a3559d6
James Munnelly <james.munnelly@gmail.com>
2015-09-26 23:48:40 +0100

    Authenticate before building

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,9 +6,9 @@ if [ $TRAVIS_PULL_REQUEST != 'false' ] || [ "$TRAVIS_BRANCH" != "master" ] || [ 
 
 else
 
+  docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
   docker build -t "munnerz/gobalancer:${TRAVIS_COMMIT:0:7}" .
   docker tag -f "munnerz/gobalancer:${TRAVIS_COMMIT:0:7}" "munnerz/gobalancer:latest"
-  docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
   docker push "munnerz/gobalancer:${TRAVIS_COMMIT:0:7}"
   docker push "munnerz/gobalancer:latest"
 
