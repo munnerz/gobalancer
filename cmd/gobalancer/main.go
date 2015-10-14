101be5f8eb9a013e762836871bcfcc7cf00d4bec
James Munnelly <james.munnelly@gmail.com>
2015-10-14 09:54:06 +0100

    Move application handler into cmd

--- cmd/gobalancer/main.go
+++ cmd/gobalancer/main.go
@@ -2,44 +2,69 @@ package main
 
 import (
 	"flag"
-	"log"
 
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/munnerz/gobalancer/pkg/addressing"
+	"github.com/munnerz/gobalancer/pkg/api"
 	"github.com/munnerz/gobalancer/pkg/config"
 	"github.com/munnerz/gobalancer/pkg/config/sources/file"
-
-	"github.com/munnerz/gobalancer"
+	svc "github.com/munnerz/gobalancer/pkg/services"
 )
 
 var (
 	configFile = flag.String("config", "config.json", "JSON formatted config file")
 
-	gb *gobalancer.GoBalancer
+	ipPool        *addressing.IPPool
+	configStorage config.Storage
+	conf          *api.Config
+
+	services    []*svc.Service
+	serviceChan = make(chan svc.Error)
 )
 
 func main() {
 	flag.Parse()
+	log.SetLevel(log.DebugLevel)
 
-	fileStorage, err := config.GetType("file")
+	// Get a config initialiser
+	NewFileStorage, err := config.GetType("file")
 
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	storage, err := fileStorage(file.SetFilename(*configFile))
+	// Initialise config FileStorage
+	cs, err := NewFileStorage(file.SetFilename(*configFile))
 
 	if err != nil {
 		log.Fatalf("Error getting config storage module: %s", err.Error())
 	}
 
-	gb, err = gobalancer.NewGoBalancer(storage)
+	configStorage = cs
+
+	// Read configuration
+	c, err := configStorage.GetConfig()
 
 	if err != nil {
-		log.Fatalf("Error initialising GoBalancer core: %s", err.Error())
+		log.Fatalf("Error getting config: %s", err)
 	}
 
-	err = gb.Run()
+	conf = c
 
-	if err != nil {
-		log.Fatalf("Error running gobalancer: %s", err.Error())
+	// Initialise IP address pool to allocate from
+	ipPool = addressing.NewIPPool(conf.IPPool)
+
+	for i, s := range conf.Services {
+		services = append(services, svc.NewService(s, serviceChan))
+		go services[i].Run()
+	}
+
+	// Wait for messages from different components...
+	for {
+		select {
+		case e := <-serviceChan:
+			log.Errorf("Error from service '%s': %s", e.Service.Name, e.Error())
+		}
 	}
 }
